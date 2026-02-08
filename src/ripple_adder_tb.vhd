8829fd4c53d5a3886e09409cfb64141dcd0988cd
cnRachel <cnRachel@users.noreply.github.com>
2026-02-08 13:52:15 -0700

    vhdl simulation passed

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,6 +49,12 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
+       w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with 0+0+cin" severity failure;
+	   w_addends <= x"A3"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"D" and w_Cout = '0') report "bad with 10+3" severity failure;
+	   w_addends <= x"2E"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "bad with 2+14" severity failure;
 	
 		wait; -- wait forever
 	end process;	
